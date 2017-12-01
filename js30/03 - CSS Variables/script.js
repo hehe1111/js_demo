5b8e7d07db7f82e8cc5faad77f49f950502c65b7
hehe1111 <2908749709@qq.com>
2017-12-01 21:14:44 +0800

    添加页面参数的实时显示

--- js30/03 - CSS Variables/script.js
+++ js30/03 - CSS Variables/script.js
@@ -3,6 +3,7 @@ const inputs = document.querySelectorAll('.controls input');
 function handleUpdate() {
   const suffix = this.dataset.sizing || ''; // 根据 data-sizing 获取参数的后缀
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix); // 设置页面 CSS 变量的值
+  document.getElementById(`code-${this.name}`).innerText = this.value + suffix; // 页面参数实时显示
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate)); // 监听滑块位置（value属性）的变化，在滑块不再被拖动时，更新页面的样式
