823ea55c08d3c12f799084c79000f70536f5ea49
hehe1111 <2908749709@qq.com>
2017-12-07 21:13:22 +0800

    更新 README.md：添加 JS 重点；为 JS 脚本添加注释；为 transition 属性添加注释。

--- js30/05 - Flex Panel Gallery/script.js
+++ js30/05 - Flex Panel Gallery/script.js
@@ -2,7 +2,7 @@ const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
   console.log('Hello');
-  this.classList.toggle('open');
+  this.classList.toggle('open'); /* 点击时，如果不存在类 open，则添加；反之，则删除已存在的类 open */
 }
 
 function toggleActive(e) {
@@ -13,4 +13,4 @@ function toggleActive(e) {
 }
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive)); /* 当 .panel 所在元素的样式转化完成时，添加/删除类 toggleActive，使其子元素即第一和最后一个 <p> 元素飞入/不应用对应样式。 */
