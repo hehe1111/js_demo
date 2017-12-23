fdd63f46485d0e4317ed3ce874bfb0de5cc9d4d9
hehe1111 <2908749709@qq.com>
2017-12-23 18:03:35 +0800

    添加另一个实现途径。

--- js30/10 - Hold Shift and Check Checkboxes/script.js
+++ js30/10 - Hold Shift and Check Checkboxes/script.js
@@ -18,4 +18,23 @@ function handleCheck1(e) {
     lastChecked = this;
 }
 
+//	原作者给的方法
+//  用变量 inBetween 对需要选中的元素进行标记
+function handleCheck0(e) {
+    let inBetween = false;
+    if(e.shiftKey && this.checked){
+        boxs.forEach(input => {
+            console.log(input);
+            if(input === lastChecked || input === this) {
+                inBetween = !inBetween;
+            }
+            if(inBetween) {
+                console.log("on");
+                input.checked = true;
+            }
+        });
+    }
+    lastChecked = this;
+}
+
 checkboxs.forEach(checkbox => checkbox.addEventListener('click', handleCheck1));
