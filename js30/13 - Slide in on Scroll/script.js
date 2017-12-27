e02117300fb1b6ff50c9b33627323546a9c35b69
hehe1111 <2908749709@qq.com>
2017-12-27 19:39:25 +0800

    添加 JS 代码。

---
+++ js30/13 - Slide in on Scroll/script.js
@@ -0,0 +1,37 @@
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    sliderImages.forEach(img => {
+        //
+        const slideInAt = (window.scrollY + window.innerHeight) - (img.height / 2);
+        // 页面顶部到图片底部的高度
+        const imgBottom = img.offsetTop + img.height;
+        // 图片显示已超过一半
+        const isHalfShown = slideInAt > img.offsetTop;
+        // 图片未完全从视口中消失
+        const isNotScrolledPast = window.scrollY < imgBottom;
+
+        if (isHalfShown && isNotScrolledPast) {
+            img.classList.add('active');
+        } else {
+            img.classList.remove('active');
+        }
+    })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
