ccddad535e5bc3cedf7bf629cc67afea6eaecb63
hehe1111 <2908749709@qq.com>
2017-12-30 21:57:59 +0800

    添加 JS。

---
+++ js30/15 - LocalStorage/script.js
@@ -0,0 +1,44 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+var items = JSON.parse(localStorage.getItem('items')) || [];
+
+
+function addItem(e) {
+    e.preventDefault(); // 取消点击提交按钮后触发的 submit 事件的默认行为：刷新整个页面
+    const text = this.querySelector('[name=item]').value; // 获取输入框中的值；this 指向 form 表单
+    const item = {
+        text: text, // ES6中可简写为 text,
+        done: false // 为后续的勾选做准备
+    }
+    items.push(item);
+    populateList(items, itemsList); // 动态显示页面内容
+    localStorage.setItem('items', JSON.stringify(items));
+    this.reset(); // 清空 input 中输入的值
+    // console.log(text);
+}
+
+function populateList(plates=[], plateslist) { // 根据输入的数据更新页面的内容
+    plateslist.innerHTML = plates.map((plate, i) => { // i 是当前位置
+        return `
+            <li>
+                <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}>
+                <label for="item${i}">${plate.text}</label>
+            </li>
+        `;
+    }).join('');
+}
+
+function toggleDone(e) {
+    if (!e.target.matches('input')) return; // skip this unless it's an input
+    const element = e.target;
+    const index = element.dataset.index;
+    items[index].done = !items[index].done; /* 修改状态：是否选中 */
+    populateList(items, itemsList); // 动态显示页面内容
+    localStorage.setItem('items', JSON.stringify(items));
+    // console.log(element.dataset.index);
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList); // 动态显示页面内容
