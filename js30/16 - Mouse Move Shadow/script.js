279de99f19e885a160b17961e91b015386ce9d46
hehe1111 <2908749709@qq.com>
2017-12-31 20:37:01 +0800

    添加 JS。

---
+++ js30/16 - Mouse Move Shadow/script.js
@@ -0,0 +1,26 @@
+var hero = document.querySelector('.hero');
+var text = hero.querySelector('h1');
+var factor = 0.4; //当鼠标移动至显示区域边界时，阴影距离占hero元素宽和高的比例
+
+hero.addEventListener('mousemove', shadowMove);
+
+function shadowMove(e) {
+    // 元素的宽高
+    var range = {
+        x: hero.offsetWidth,
+        y: hero.offsetHeight
+    }
+
+    // 鼠标的位置
+    var position = {
+        x: e.target.offsetLeft + e.offsetX,
+        y: e.target.offsetTop + e.offsetY
+    }
+
+    // 计算阴影移动的距离
+    var distanceX = parseInt((position.x - (range.x / 2)) * factor);
+    var distanceY = parseInt((position.y - (range.y / 2)) * factor);
+
+    // 修改阴影样式
+    text.style.textShadow = distanceX + 'px ' + distanceY + 'px 0 #3498db, ' + (-distanceX) + 'px ' + distanceY + 'px 0 #1abc9c, ' + distanceY + 'px ' + (-distanceX) + 'px 0 #9b59b6, ' + (-distanceY) + 'px ' + (-distanceX) + 'px 0 #e74c3c';
+}
