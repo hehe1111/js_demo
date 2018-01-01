6de85a1d06d692ba939afec99af18bd5e3b42235
hehe1111 <2908749709@qq.com>
2018-01-01 20:02:03 +0800

    注释文字前加一个空格。

--- js30/17 - Sort Without Articles/script.js
+++ js30/17 - Sort Without Articles/script.js
@@ -3,12 +3,12 @@ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
     'We Came as Romans', 'Counterparts', 'Oh, Sleeper','A Skylit Drive',
     'Anywhere But Here', 'An Old Dog'];
 
-//去前缀函数
+// 去前缀函数
 function delPrefix(item) {
     return item.replace(/^(The|An|A)\s{1}/, '');
 }
 
-//展示至HTML页面
+// 展示至HTML页面
 function displayArray(arr) {
     document.getElementById('bands').innerHTML = (
         '<li>' + arr.join('</li><li>') + 'li'); // 加括号是为了换行
@@ -31,7 +31,7 @@ function displayArray2(arr) {
     document.getElementById('bands').appendChild(docFrag);
 }
 
-//按要求排序，先大后小
+// 按要求排序，先大后小
 bands.sort(function(a, b) {
     return delPrefix(a) > delPrefix(b) ? 1 : -1; // 去除前缀后再进行比较，但最终并不会改变数组元素
 });
