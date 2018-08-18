5b3a3fea29fc2d546e8d4c8fe090365a46644f66
hehe1111 <2908749709@qq.com>
2018-08-18 17:55:56 +0800

    调整缩进。

--- router-demo/server.js
+++ router-demo/server.js
@@ -4,35 +4,33 @@ var url = require('url')
 var port = process.argv[2]
 
 if (!port) {
-  console.log('请指定端口号，例如\nnode server.js 8080')
-  process.exit(1)
+    console.log('请指定端口号，例如\nnode server.js 8080')
+    process.exit(1)
 }
 
 var server = http.createServer(function (request, response) {
-  var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
-  var method = request.method
+    var parsedUrl = url.parse(request.url, true)
+    var path = request.url
+    var query = ''
+    if (path.indexOf('?') >= 0) { query = path.substring(path.indexOf('?')) }
+    var pathNoQuery = parsedUrl.pathname
+    var queryObject = parsedUrl.query
+    var method = request.method
 
-  console.log('当前 HTTP 相对路径为\n' + path)
-  if(path === '/' || path === '/tab1' || path === '/tab2'){
-    var string = fs.readFileSync('./router-demo.html', 'utf8')
-    response.setHeader('Content-Type', 'text/html; charset=utf-8')
-    response.statusCode = 200
-    response.write(string)
-    response.end()
-  } else {
-    response.statusCode = 404
-    response.write(`
-    {
-      "error": 'Not Found'
+    console.log('当前 HTTP 相对路径为\n' + path)
+    if (path === '/' || path === '/tab1' || path === '/tab2') {
+        var string = fs.readFileSync('./router-demo.html', 'utf8')
+        response.setHeader('Content-Type', 'text/html; charset=utf-8')
+        response.statusCode = 200
+        response.write(string)
+        response.end()
+    } else {
+        response.statusCode = 404
+        response.write(`{
+            "error": 'Not Found'
+        }`)
+        response.end()
     }
-    `)
-    response.end()
-  }
 })
 
 server.listen(port)
